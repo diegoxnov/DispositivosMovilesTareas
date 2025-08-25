1b8a38a4dc7630c02e26dbe297e0430ef285d768
diegoNovaR <diegonova368@gmail.com>
2025-08-24 21:04:56 -0500

    Terminando el main y corrigiendo funciones

--- Practica_1/src/Eval_Empleado.kt
+++ Practica_1/src/Eval_Empleado.kt
@@ -1,18 +1,32 @@
-fun CalculoSalario(puntuacion: Int, salario: Float): Float {
+import kotlin.Float
+
+fun CalculoSalario(puntuacion: Float, salario: Float): Float {
     val dinero = salario * (puntuacion/10)
     return dinero
 }
 
-fun NivelRendimiento(puntuacion: Int): String {
-    if (puntuacion > 0 && puntuacion < 6) {
+fun NivelRendimiento(puntuacion: Float): String {
+    if (puntuacion >= 0 && puntuacion <= 3) {
         return "Inaceptable"
-    } else if (puntuacion > 5 && puntuacion < 7) {
+    } else if (puntuacion >= 4 && puntuacion <= 6) {
         return "Aceptable"
-    } else if (puntuacion > 6 && puntuacion < 11) {
+    } else if (puntuacion >= 7 && puntuacion <= 10) {
         return "Meritorio"
     } else {
         return "Puntuacion Invalida"
     }
 }
 
-
+fun main(){
+    println("Bienvenido")
+    println("Ingresa el salario del empleado")
+    val salario = readLine()!!.toFloat()
+    println("Ingresa la puntuación del empleado del 0 al 10")
+    val puntuacion = readLine()!!.toFloat()
+    if (puntuacion > 0 && puntuacion < 10) {
+        val salarioTotal = CalculoSalario(puntuacion, salario)
+        println("Salario inicial: $salario; puntuacion: $puntuacion; Dinero: ${CalculoSalario(puntuacion,salario)} Resultado del rendimiento: ${NivelRendimiento(puntuacion)} ")
+    } else if (puntuacion < 0 || puntuacion > 10) {
+        println("Puntuacion Invalida")
+    }
+}
