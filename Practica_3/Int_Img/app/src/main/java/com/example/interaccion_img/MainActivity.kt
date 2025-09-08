2ec32c7ea5b8f00b1718792f601644bff7408cce
diegoNovaR <diegonova368@gmail.com>
2025-09-08 10:18:08 -0500

    Agregando comentarios al trabajo

--- Practica_3/Int_Img/app/src/main/java/com/example/interaccion_img/MainActivity.kt
+++ Practica_3/Int_Img/app/src/main/java/com/example/interaccion_img/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.interaccion_img
-
+//AUTOR DIEGO NOVA
+// FECHA 08/09/2025
+// FECHA ULTIMA EDICION 08/09/2025
+// HACER CHICL A UNA IMAGEN Y MOSTRAR UN TOAST OCN MENSAJE
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageView
@@ -16,8 +19,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //Creando la variable de la img
         val idImg = findViewById<ImageView>(R.id.imgIntegra)
-        idImg.setOnClickListener {
-            Toast.makeText(this, "Hola soy un Honda Integra gsr", Toast.LENGTH_SHORT).show()
+        idImg.setOnClickListener {//accion de hacer click a la imagen
+            Toast.makeText(this, "Hola soy un Honda Integra gsr", Toast.LENGTH_SHORT).show() // ejecutando el toast despues de hacer clicl en la imagen
         }
     }
 }
