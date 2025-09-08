f8de1b1c71af9d8824eaa4143ca4bbe2110ae6aa
diegoNovaR <diegonova368@gmail.com>
2025-09-08 10:16:03 -0500

    terminando el trabajo interaccion con imagen

--- Practica_3/Int_Img/app/src/main/java/com/example/interaccion_img/MainActivity.kt
+++ Practica_3/Int_Img/app/src/main/java/com/example/interaccion_img/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.interaccion_img
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +12,12 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        //Creando la variable de la img
+        val idImg = findViewById<ImageView>(R.id.imgIntegra)
+        idImg.setOnClickListener {
+            Toast.makeText(this, "Hola soy un Honda Integra gsr", Toast.LENGTH_SHORT).show()
         }
     }
 }
