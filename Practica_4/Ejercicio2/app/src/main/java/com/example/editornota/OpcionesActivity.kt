db53aa1ac6bdf57231b958db91e8a32d1afff658
Diego Nova <dnovar@ulasalle.edu.pe>
2025-09-25 22:29:43 -0500

    Obteniendo los botenes y declarando txtNota para restaurar sus cambios luego

--- Practica_4/Ejercicio2/app/src/main/java/com/example/editornota/OpcionesActivity.kt
+++ Practica_4/Ejercicio2/app/src/main/java/com/example/editornota/OpcionesActivity.kt
@@ -1,16 +1,26 @@
 package com.example.editornota
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class OpcionesActivity : AppCompatActivity() {
+
+    private lateinit var txtNota: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_opciones)
+
+        val btnCorreo = findViewById<Button>(R.id.btnCorreo)
+        val btnEdit = findViewById<Button>(R.id.btnEdit)
+
 
     }
 }
