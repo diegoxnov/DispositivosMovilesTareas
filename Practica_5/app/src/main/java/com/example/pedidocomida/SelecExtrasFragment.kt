44061ac7e5adedcba2ed6753932d7548c6b3c50e
Diego Nova <dnovar@ulasalle.edu.pe>
2025-09-29 11:33:08 -0500

    Agregando logica de botonNext, rescatando los datos seleccionados y enviando al resumenFragment

--- Practica_5/app/src/main/java/com/example/pedidocomida/SelecExtrasFragment.kt
+++ Practica_5/app/src/main/java/com/example/pedidocomida/SelecExtrasFragment.kt
@@ -8,19 +8,39 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.RadioGroup
 import android.widget.TextView
+import androidx.core.os.bundleOf
 
 class SelecExtrasFragment : Fragment(R.layout.fragment_selec_extras) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val btnNext = view.findViewById<Button>(R.id.btnExtrasNext)
+        val btnBack = view.findViewById<Button>(R.id.btnExtrasBack)
         val rgExtra = view.findViewById<RadioGroup>(R.id.radioGroupExtra)
         val txtProceso = view.findViewById<TextView>(R.id.textViewProceso)
 
         val comida = arguments?.getString("comida") ?: ""// obtenemos la comida del fragment comida
         txtProceso.text = "Tu selección de comida es: $comida"
 
+        btnNext.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction().apply {
+                val extraSelect = when(rgExtra.checkedRadioButtonId){
+                    R.id.rbPapa -> "papa"
+                    R.id.rbBebida -> "bebida"
+                    R.id.rbPostre -> "postre"
+                    else -> "sin extras"
+                }
 
+                val datosPostreComida = bundleOf("comida" to comida, "postre" to extraSelect)
+                val resumenFragment = ResumenPedidoFragment()
+                resumenFragment.arguments = datosPostreComida
+
+
+                replace(R.id.fragmentContainerInicio, resumenFragment)
+                addToBackStack("DatosComidaExtra")
+                commit()
+            }
+        }
 
     }
 }
