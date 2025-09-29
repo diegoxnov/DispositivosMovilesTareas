f0e4e33f26903fb0660c5094219e7cfa03c388f7
Diego Nova <dnovar@ulasalle.edu.pe>
2025-09-28 20:04:19 -0500

    Agregando logica de boton inicio

--- Practica_5/app/src/main/java/com/example/pedidocomida/InicioFragment.kt
+++ Practica_5/app/src/main/java/com/example/pedidocomida/InicioFragment.kt
@@ -5,9 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 
 class InicioFragment : Fragment(R.layout.fragment_inicio) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnPedido = view.findViewById<Button>(R.id.btnNuevoPedido)
+
+        btnPedido.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragmentContainerInicio, SelecComidaFragment())
+                addToBackStack("DatosComida")
+                commit()
+            }
+        }
+
+    }
 
 }
