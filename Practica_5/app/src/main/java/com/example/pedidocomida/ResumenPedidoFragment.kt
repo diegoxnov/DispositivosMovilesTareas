66c624e9b479c838a52320cb23daef88cfcaf922
Diego Nova <dnovar@ulasalle.edu.pe>
2025-09-29 11:34:42 -0500

    Agregando nuevo fragment Resumen

---
+++ Practica_5/app/src/main/java/com/example/pedidocomida/ResumenPedidoFragment.kt
@@ -0,0 +1,12 @@
+package com.example.pedidocomida
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class ResumenPedidoFragment : Fragment(R.layout.fragment_resumen_pedido) {
+
+}
