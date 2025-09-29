4dfb322a6b8fbdbb62cbb0d17ee4cdd4e36e3a84
Diego Nova <dnovar@ulasalle.edu.pe>
2025-09-29 11:16:49 -0500

    Agregando logica para rescatar y enviar la info a selecExtrasFragment

--- Practica_5/app/src/main/java/com/example/pedidocomida/SelecComidaFragment.kt
+++ Practica_5/app/src/main/java/com/example/pedidocomida/SelecComidaFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.RadioGroup
+import androidx.core.os.bundleOf
 
 
 class SelecComidaFragment : Fragment(R.layout.fragment_selec_comida) {
@@ -13,12 +15,27 @@ class SelecComidaFragment : Fragment(R.layout.fragment_selec_comida) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val btnNext = view.findViewById<Button>(R.id.btnSiguienteComida)
+        val rgComida = view.findViewById<RadioGroup>(R.id.radioGroupComida)//obtenemos el valor del xml
+
 
         btnNext.setOnClickListener {
+
             requireActivity().supportFragmentManager.beginTransaction().apply {
-                replace(R.id.fragmentContainerInicio, SelecExtrasFragment())
-                addToBackStack("DatosComida")
-                commit()
+                val comidaSelec = when(rgComida.checkedRadioButtonId){
+                    R.id.rbEnsalada -> "Ensalada"
+                    R.id.rbPizza -> "Pizza"
+                    R.id.rbHamburguesa -> "Hamburguesa"
+                    else -> "sin comida"
+                }
+
+                val datosComida = bundleOf("comida" to comidaSelec)
+                val extraFragment = SelecExtrasFragment()
+                extraFragment.arguments = datosComida
+
+
+                    replace(R.id.fragmentContainerInicio, extraFragment)
+                    addToBackStack("DatosComida")
+                    commit()
             }
         }
 
