ce3180e3e5c220f1eafbd06582354519ff120219
Diego Nova <dnovar@ulasalle.edu.pe>
2025-09-28 20:47:22 -0500

    Agregando interface y logica al fragment SELECCOMIDA

--- Practica_5/app/src/main/java/com/example/pedidocomida/SelecComidaFragment.kt
+++ Practica_5/app/src/main/java/com/example/pedidocomida/SelecComidaFragment.kt
@@ -5,10 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 
 class SelecComidaFragment : Fragment(R.layout.fragment_selec_comida) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnNext = view.findViewById<Button>(R.id.btnSiguienteComida)
 
+        btnNext.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragmentContainerInicio, SelecComidaFragment())
+                addToBackStack("DatosComida")
+                commit()
+            }
+        }
+
+    }
 
 }
