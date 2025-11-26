256b69bd7144bd26bc6ae7e5ce652536f540a2b8
Diego Nova <diegonova368@gmail.com>
2025-11-26 11:05:45 -0500

    Modificando modelo

--- flutter_provider/lib/models/user.dart
+++ flutter_provider/lib/models/user.dart
@@ -2,10 +2,14 @@ class User {
   String nombre;
   String genero; // 'Masculino' o 'Femenino'
   bool activo;
+  int edad;
+  String correo;
 
   User({
     required this.nombre,
     required this.genero,
     required this.activo,
+    required this.edad,
+    required this.correo,
   });
 }
